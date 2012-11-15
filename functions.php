06e10a5ed6efdd4f79a48f74122fc3b6783dfe42
Chris Conover <clcono@gmail.com>
2012-11-15 14:55:31 -0500

    Remove admin dashboard menus

--- functions.php
+++ functions.php
@@ -7,5 +7,26 @@ require_once('functions/config.php');			# Where per theme settings are registere
 require_once('shortcodes.php');         		# Per theme shortcodes
 
 //Add theme-specific functions here.
-
+function remove_menus () {
+	global $menu;
+	$restricted = array(
+		//__('Dashboard'),
+		__('Posts'),
+		__('Media'),
+		__('Links'),
+		__('Pages'),
+		__('Appearance'),
+		__('Tools'),
+		__('Users'),
+		__('Settings'),
+		__('Comments'),
+		__('Plugins')
+	);
+	end ($menu);
+	while (prev($menu)){
+		$value = explode(' ',$menu[key($menu)][0]);
+		if(in_array($value[0] != NULL?$value[0]:"" , $restricted)){unset($menu[key($menu)]);}
+	}
+}
+add_action('admin_menu', 'remove_menus');
 ?>
