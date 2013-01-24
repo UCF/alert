2e60cb5749debcf202b006a8fac46104e82ad58a
Jo Greybill <jogreybill@gmail.com>
2013-01-24 16:08:56 -0500

    Added alert text truncating

--- feeds/feed-alert-rss2.php
+++ feeds/feed-alert-rss2.php
@@ -4,7 +4,7 @@
  *
  * @package WordPress
  */
-
+ 
 header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_option('blog_charset'), true);
 $more = 1;
 
@@ -32,6 +32,7 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 	<?php 
 	$theme_options  = get_option(THEME_OPTIONS_NAME);
 	$expiration		= $theme_options['outgoing_expiration'] ? (int)$theme_options['outgoing_expiration'] : 60;
+	$alert_length	= $theme_options['outgoing_text_length'] ? (int)$theme_options['outgoing_text_length'] : 250;
 	
 	$args = array(
 		'post_type'			=> 'alert',
@@ -45,7 +46,10 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 	
 	if ( date('YmdHis', strtotime($post->post_modified)) >= date('YmdHis', strtotime('-'.$expiration.' minutes')) ) {
 		$short = get_post_meta($post->ID, 'alert_short', True);
-		if($short != '') {
+		if($short != '') {			
+			// Truncate by text length, then remove the last full/partial word
+			$short = substr($short, 0, $alert_length);
+			$short = preg_replace('/ [^ ]*$/', ' ...', $short);
 	?>
 		<item>
 			<title><?php the_title_rss() ?></title>
@@ -59,9 +63,9 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 	
 			<guid isPermaLink="false"><?php the_guid(); ?></guid>
 	<?php if (get_option('rss_use_excerpt')) : ?>
-			<description><![CDATA[<?php echo get_post_meta($post->ID, 'alert_short', True); ?>]]></description>
+			<description><![CDATA[<?php echo $short; ?>]]></description>
 	<?php else : ?>
-			<description><![CDATA[<?php echo get_post_meta($post->ID, 'alert_short', True); ?>]]></description>
+			<description><![CDATA[<?php echo $short; ?>]]></description>
 		<?php $content = get_the_content_feed('rss2'); ?>
 		<?php if ( strlen( $content ) > 0 ) : ?>
 			<content:encoded><![CDATA[<?php echo $content; ?>]]></content:encoded>
