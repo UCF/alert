413fb3472b5e60059b42e640ae831813008e7bbd
Jo Greybill <jogreybill@gmail.com>
2013-01-28 08:53:16 -0500

    Make sure to only truncate alert text if its string length exceeds the set alert text length

--- feeds/feed-alert-rss2.php
+++ feeds/feed-alert-rss2.php
@@ -30,9 +30,9 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 	<?php do_action('rss2_head'); ?>
 	
 	<?php 
-	$theme_options  = get_option(THEME_OPTIONS_NAME);
-	$expiration		= $theme_options['outgoing_expiration'] ? (int)$theme_options['outgoing_expiration'] : 60;
-	$alert_length	= $theme_options['outgoing_text_length'] ? (int)$theme_options['outgoing_text_length'] : 250;
+	$theme_options    = get_option(THEME_OPTIONS_NAME);
+	$expiration		  = $theme_options['outgoing_expiration'] ? (int)$theme_options['outgoing_expiration'] : 60;
+	$def_alert_length = $theme_options['outgoing_text_length'] ? (int)$theme_options['outgoing_text_length'] : 350;
 	
 	$args = array(
 		'post_type'			=> 'alert',
@@ -47,9 +47,14 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 	if ( date('YmdHis', strtotime($post->post_modified)) >= date('YmdHis', strtotime('-'.$expiration.' minutes')) ) {
 		$short = get_post_meta($post->ID, 'alert_short', True);
 		if($short != '') {			
+			// We only want to truncate to the max alert length if the actual alert
+			// text length exceeds that max value.
+			$needs_truncating = $def_alert_length < strlen($short) ? true : false; 
 			// Truncate by text length, then remove the last full/partial word
-			$short = substr($short, 0, $alert_length);
-			$short = preg_replace('/ [^ ]*$/', ' ...', $short);
+			if ($needs_truncating == true) {
+				$short = substr($short, 0, $def_alert_length);
+				$short = preg_replace('/ [^ ]*$/', ' ...', $short);
+			}
 	?>
 		<item>
 			<title><?php the_title_rss() ?></title>
